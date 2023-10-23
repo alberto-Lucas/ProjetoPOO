10676fd85fa64ac4ee68e52d997f69e631eb33cb
alberto-Lucas <100029506+alberto-Lucas@users.noreply.github.com>
2023-10-23 10:46:39 -0300

    Aula 20/10/2023
    Aula 20/10/2023

---
+++ ProjetoPOO/Models/PedidoItemCollection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjetoPOO.Models
+{
+    public class PedidoItemCollection : List<PedidoItem>
+    {
+    }
+}
---
+++ ProjetoPOO/Services/DataBaseSqlServer.cs
@@ -0,0 +1,24 @@
+﻿//É preciso importar as bibliotecas do SQLSERVER
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ProjetoPOO.Services
+{
+    //Métodos privado que cria nova conexão com o banco
+    private SqlConnection CriarConexao()
+    {
+        SqlConnection conexao = new SqlConnection();
+
+        conexao.ConnectionString =
+            "Data Source=localhost;" +
+            "Initial Catalog=pooCamadas;" +
+            "Integrated Security=SSPI;";
+
+        conexao.Open();
+        return conexao;
+    }
+
+    public class DataBaseSqlServer
+    {
+    }
+}
