e89cca565c1707c649b42c9b3b5ef76c684531c7
alberto-Lucas <100029506+alberto-Lucas@users.noreply.github.com>
2023-10-27 22:22:06 -0300

    Aula dia 27/10/2023
    ClienteController - Em Progresso

--- ProjetoPOO/Controllers/ClienteController.cs
+++ ProjetoPOO/Controllers/ClienteController.cs
@@ -2,10 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using ProjetoPOO.Services;
 
 namespace ProjetoPOO.Controllers
 {
-    class ClienteController
+    public class ClienteController
     {
+        #region Variaveis Locais
+        //Variavel local e privada que faz o acesso ao 
+        //banco de dados e executa os comandos.
+        DataBaseSqlServer dataBase 
+            = new DataBaseSqlServer();
+        #endregion
+
+        #region Inserir
+        //Método puliboc que insere na tabela de clientes
+        //um novo registro. Os dados que serão inseridos
+        //são passados no parametro atraves de um objeto
+        //do tipo Cliente. Os dados foram preenchidos pelo
+        //usuário na tela de cadastro de clientes.
+
+        #endregion
+
     }
 }
